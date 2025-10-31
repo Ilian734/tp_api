39eda4724bb4f17dbeab59b1048943ab9a68c44d
Ilian734 <146117555+Ilian734@users.noreply.github.com>
2025-10-31 12:50:48 +0100

    Add files via upload

---
+++ config.mjs
@@ -0,0 +1,13 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://ilian:0000@cluster0.ujaaysz.mongodb.net/tp3?retryWrites=true&w=majority'
+  },
+ 
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://ilian:0000@cluster0.ujaaysz.mongodb.net/tp3?retryWrites=true&w=majority'
+  }
+};
